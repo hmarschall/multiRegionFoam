37b74b8b86ae5ded3d2cd1e8966c9971b890e47a
Constantin Habes <constantin.habes@tu-darmstadt.de>
2023-04-11 09:06:49 +0200

    [MOD]: Refactored regionInterface, child classes, and mapping procedure to adapt to interface velocity computation

--- src/regionInterfaces/capillaryInterface/capillaryInterface.C
+++ src/regionInterfaces/capillaryInterface/capillaryInterface.C
@@ -37,22 +37,22 @@ namespace regionInterfaces
 
     addToRunTimeSelectionTable
     (
-        regionInterface, 
-        capillaryInterface, 
-        IOdictionary 
+        regionInterface,
+        capillaryInterface,
+        IOdictionary
     );
 }
 }
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
-    
+
 Foam::regionInterfaces::capillaryInterface::capillaryInterface
 (
     const word& type,
     const dictionary& dict,
-    const Time& runTime,   
-    const fvPatch& patchA, 
-    const fvPatch& patchB  
+    const Time& runTime,
+    const fvPatch& patchA,
+    const fvPatch& patchB
 )
 :
     regionInterface(type, dict, runTime, patchA, patchB),
@@ -79,21 +79,287 @@ Foam::regionInterfaces::capillaryInterface::capillaryInterface
             sigma0_,
             zeroGradientFaPatchScalarField::typeName
         )
-    )
+    ),
+
+    UsPtr_(),
+    phisPtr_()
 {}
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
+void Foam::regionInterfaces::capillaryInterface::clearOut() const
+{
+    UsPtr_.clear();
+    phisPtr_.clear();
+
+    regionInterface::clearOut();
+}
+
+void Foam::regionInterfaces::capillaryInterface::makeUs() const
+{
+    if (!UsPtr_.empty())
+    {
+        FatalErrorIn("regionInterface::makeUs()")
+            << "surface velocity field already exists"
+            << abort(FatalError);
+    }
+
+    // Set patch field types for Us
+    wordList patchFieldTypes
+    (
+        aMesh().boundary().size(),
+        zeroGradientFaPatchVectorField::typeName
+    );
+
+    forAll(aMesh().boundary(), patchI)
+    {
+        if
+        (
+            aMesh().boundary()[patchI].type()
+         == wedgeFaPatch::typeName
+        )
+        {
+            patchFieldTypes[patchI] =
+                wedgeFaPatchVectorField::typeName;
+        }
+        else
+        {
+            label ngbPolyPatchID =
+                aMesh().boundary()[patchI].ngbPolyPatchIndex();
+
+            if (ngbPolyPatchID != -1)
+            {
+                if
+                (
+                    meshA().boundary()[ngbPolyPatchID].type()
+                 == wallFvPatch::typeName
+                )
+                {
+                    WarningIn("regionInterface::makeUs() const")
+                        << "Patch neighbouring to interface is wall" << nl
+                        << "Not appropriate for inlets/outlets" << nl
+                        << endl;
+
+                    patchFieldTypes[patchI] =
+                        slipFaPatchVectorField::typeName;
+                }
+            }
+        }
+    }
+
+    // Set surface velocity
+    UsPtr_.reset
+    (
+        new areaVectorField
+        (
+            IOobject
+            (
+                patchA().name() + "Us",
+                runTime().timeName(),
+                meshA(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            aMesh(),
+            dimensioned<vector>("Us", dimVelocity, vector::zero),
+            patchFieldTypes
+        )
+    );
+}
+
+void Foam::regionInterfaces::capillaryInterface::makePhis() const
+{
+    if (!phisPtr_.empty())
+    {
+        FatalErrorIn("regionInterface::makePhis()")
+            << "surface fluid flux already exists"
+            << abort(FatalError);
+    }
+
+    phisPtr_.reset
+    (
+        new edgeScalarField
+        (
+            IOobject
+            (
+                patchA().name() + "Phis",
+                runTime().timeName(),
+                meshA(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            linearEdgeInterpolate(Us()) & aMesh().Le()
+        )
+    );
+}
+
+void Foam::regionInterfaces::capillaryInterface::correctUsBoundaryConditions()
+{
+    const volVectorField& U = meshA().lookupObject<volVectorField>("U");
+
+    forAll(Us().boundaryField(), patchI)
+    {
+        if
+        (
+            UsPtr_().boundaryField()[patchI].type()
+         == calculatedFaPatchVectorField::typeName
+        )
+        {
+            vectorField& pUs = Us().boundaryField()[patchI];
+
+            pUs = Us().boundaryField()[patchI].patchInternalField();
+
+            label ngbPolyPatchID =
+                aMesh().boundary()[patchI].ngbPolyPatchIndex();
+
+            if (ngbPolyPatchID != -1)
+            {
+                if
+                (
+                    (
+                        U.boundaryField()[ngbPolyPatchID].type()
+                     == slipFvPatchVectorField::typeName
+                    )
+                 ||
+                    (
+                        U.boundaryField()[ngbPolyPatchID].type()
+                     == symmetryFvPatchVectorField::typeName
+                    )
+                )
+                {
+                    vectorField N
+                    (
+                        aMesh().boundary()[patchI].ngbPolyPatchFaceNormals()
+                    );
+
+                    pUs -= N*(N&pUs);
+                }
+            }
+        }
+    }
+
+    Us().correctBoundaryConditions();
+}
+
+void Foam::regionInterfaces::capillaryInterface::updateUs()
+{
+    if (!meshA().foundObject<volVectorField>("U"))
+    {
+        return;
+    }
+
+    // Get data needed for Us calculation
+    vectorField nA = meshA().boundary()[patchAID()].nf();
+
+    const volVectorField& UA = meshA().lookupObject<volVectorField>("U");
+    vectorField UAp = UA.boundaryField()[patchAID()].patchInternalField();
+
+    const volVectorField& UB = meshB().lookupObject<volVectorField>("U");
+    vectorField UBp = interpolateFacesFromB
+        (
+            UB.boundaryField()[patchBID()].patchInternalField()()
+        );
+
+    const surfaceScalarField& phi = meshA()
+        .lookupObject<surfaceScalarField>("phi");
+    scalarField phiP = phi.boundaryField()[patchAID()];
+
+    dimensionedScalar muA
+        (
+            meshA().lookupObject<IOdictionary>("transportProperties")
+            .lookup("mu")
+        );
+
+    dimensionedScalar muB
+        (
+            meshB().lookupObject<IOdictionary>("transportProperties")
+            .lookup("mu")
+        );
+
+    scalarField DnA = meshA().boundary()[patchAID()].deltaCoeffs();
+
+    scalarField DnB = interpolateFacesFromB
+        (
+            meshB().boundary()[patchBID()].deltaCoeffs()
+        );
+
+
+    // Calculate Us
+    vectorField UtPA = UAp;
+    UtPA -= nA*(nA & UtPA);
+
+    vectorField UtPB = UBp;
+    UtPB -= nA*(nA & UtPB);
+
+    vectorField UnFs =nA*phiP/meshA().boundary()[patchAID()].magSf();
+
+    Us().internalField() += UnFs - nA*(nA&Us().internalField());
+    correctUsBoundaryConditions();
+
+    vectorField UtFs =
+        muA.value()*DnA*UtPA
+      + muB.value()*DnB*UtPB
+      + (muB.value() - muA.value())
+       *(fac::grad(Us())&aMesh().faceAreaNormals())().internalField()
+      + tangentialSurfaceTensionForce();
+
+    UtFs /= muA.value()*DnA + muB.value()*DnB;
+
+    Us().internalField() = UnFs + UtFs;
+
+}
+
+void Foam::regionInterfaces::capillaryInterface::updatePhis()
+{
+    Phis() = fac::interpolate(Us()) & aMesh().Le();
+}
+
+tmp<vectorField>
+Foam::regionInterfaces::capillaryInterface::surfaceTensionForce() const
+{
+    return
+    (
+        sigma_
+       *fac::edgeIntegrate
+        (
+            aMesh().Le()
+            *aMesh().edgeLengthCorrection()
+        )().internalField()
+    );
+}
+
+tmp<vectorField>
+Foam::regionInterfaces::capillaryInterface::tangentialSurfaceTensionForce() const
+{
+    vectorField nA = meshA().boundary()[patchAID()].nf();
+
+    return
+    (
+        surfaceTensionForce()
+      - normalSurfaceTensionForce()
+    );
+}
+
+tmp<vectorField>
+Foam::regionInterfaces::capillaryInterface::normalSurfaceTensionForce() const
+{
+    vectorField nA = meshA().boundary()[patchAID()].nf();
+
+    return
+    (
+        sigma_*aMesh().faceCurvatures().internalField()*nA
+    );
+}
+
 void Foam::regionInterfaces::capillaryInterface::correct()
 {
     // Update transport properties
-    updateK();
     updateUs();
     updatePhis();
 
     // Update interface physics
-    // TODO: call function to calculate new sigma_ with 
-    // interface equation of state for contaminated surfaces 
+    // TODO: call function to calculate new sigma_ with
+    // interface equation of state for contaminated surfaces
 }
 
 Foam::scalar Foam::regionInterfaces::capillaryInterface::getMinDeltaT()
