56dd8ace4656ca12eac14e5ed40e5220e953b63d
Constantin Habes <constantin.habes@tu-darmstadt.de>
2023-05-30 11:20:07 +0200

    Revert "[FIX]: Temporarily fixed parallel communication problems in multiRegionFoam"
    This reverts commit eb5756924b09a30e78fa921366689b20b9bca362.

--- src/globalPolyPatch/globalPolyPatch.C
+++ src/globalPolyPatch/globalPolyPatch.C
@@ -87,9 +87,6 @@ void Foam::globalPolyPatch::calcGlobalPatch() const
             mesh_.boundaryMesh()[patchID.index()].localFaces();
     }
 
-    label oldMsgType = Pstream::msgType();
-    Pstream::msgType() = 33;
-
     // Communicate points
     Pstream::gatherList(procPatchPoints);
     Pstream::scatterList(procPatchPoints);
@@ -97,8 +94,6 @@ void Foam::globalPolyPatch::calcGlobalPatch() const
     // Communicate faces
     Pstream::gatherList(procPatchFaces);
     Pstream::scatterList(procPatchFaces);
-
-    Pstream::msgType() = oldMsgType;
 
     // At this point, all points and faces for the current patch
     // are available.
@@ -226,10 +221,10 @@ void Foam::globalPolyPatch::calcGlobalPatch() const
 
     if (debug)
     {
-        Pout << nl
+        Pout << nl 
              << "nDuplicatePoints: " << nDuplicatePoints
              << " on global patch: " << patchName_ << nl
-             << "resizing zonePoints from "
+             << "resizing zonePoints from " 
              << zonePoints.size() << " to " << nCurPoints << endl;
     }
 
